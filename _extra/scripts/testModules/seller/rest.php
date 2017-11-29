04472af21b49c847e549c17c47faa92d673b21bf
Eric Marchal <e.marchal@truffaut.com>
2017-11-29 17:06:27 +0100

    seller model

---
+++ _extra/scripts/testModules/seller/rest.php
@@ -0,0 +1,39 @@
+<?php
+require __DIR__.'/../_tools/init.php';
+
+// Initialize the client
+$client = new \SmileCoreTest\RestClient();
+$client->setDebug(true);
+$client->setMagentoParams($params);
+$client->connect();
+
+$object=[
+    'seller' => [
+        'identifier' => 'test',
+        'name'       => 'my seller'
+    ]
+];
+$search = [
+    'searchCriteria' => [
+        'filterGroups' => [
+            [
+                'filters' => [
+                    [
+                        'field'          =>'identifier',
+                        'condition_type' => 'like',
+                        'value'          => '%es%'
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
+
+$client->post('rest/V1/seller/',$object);
+$client->get('rest/V1/seller/identifier/'.$object['seller']['identifier']);
+$client->delete('rest/V1/seller/identifier/'.$object['seller']['identifier']);
+
+$ps = $client->post('rest/V1/seller/',$object);
+$client->get('rest/V1/seller/id/'.$ps['seller_id']);
+$client->get('rest/V1/seller?'.http_build_query($search));
+$client->delete('rest/V1/seller/id/'.$ps['seller_id']);
---
+++ _extra/scripts/testModules/seller/soap.php
@@ -0,0 +1,42 @@
+<?php
+require __DIR__.'/../_tools/init.php';
+
+// Initialize the client
+$client = new \SmileCoreTest\SoapClient();
+$client->setDebug(true);
+$client->setMagentoParams($params);
+$client->addService('trainingSellerSellerRepositoryV1');
+
+$object=[
+    'seller' => [
+        'identifier' => 'test',
+        'name'       => 'my seller'
+    ]
+];
+$search = [
+    'searchCriteria' => [
+        'filterGroups' => [
+            [
+                'filters' => [
+                    [
+                        'field'          =>'identifier',
+                        'conditionType' => 'like',
+                        'value'          => '%es%'
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
+
+$client->trainingSellerSellerRepositoryV1Save($object);
+$client->trainingSellerSellerRepositoryV1GetByIdentifier(['identifier' => 'test']);
+$client->trainingSellerSellerRepositoryV1DeleteByIdentifier(['identifier' => 'test']);
+
+$ps = $client->trainingSellerSellerRepositoryV1Save($object);
+$client->trainingSellerSellerRepositoryV1GetById(['sellerId' => $ps->sellerId]);
+$client->trainingSellerSellerRepositoryV1GetList($search);
+$client->trainingSellerSellerRepositoryV1DeleteById(['sellerId' => $ps->sellerId]);
+
+
+
