a2d53f3cc01f5df71adbe0147468ecf0db751db5
Eric Marchal <e.marchal@truffaut.com>
2017-12-01 16:23:22 +0100

    seller model

---
+++ app/code/Training/Seller/Console/GetCommand.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: training
+ * Date: 12/1/17
+ * Time: 3:49 PM
+ */
+
+namespace Training\Seller\Console;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Training\Seller\Api\SellerRepositoryInterface;
+
+
+class GetCommand extends Command
+{
+
+    const ID_OPTION='id';
+    /**
+     * @var SellerRepositoryInterface
+     */
+    private $sellerRepository;
+
+    /**
+     * GetCommand constructor.
+     * @param SellerRepositoryInterface $sellerRepository
+     * @param null $name
+     */
+    public function __construct(SellerRepositoryInterface $sellerRepository, $name = null)
+    {
+        parent::__construct();
+        $this->sellerRepository = $sellerRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('training:seller:get')
+        ->setDescription('Display seller name')
+        ->setDefinition([
+            new InputOption(
+                self::ID_OPTION,
+                '-i',
+                InputOption::VALUE_REQUIRED,
+                'Seller id'
+            ),
+
+        ]);
+
+        parent::configure();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $sellerId = $input->getOption(self::ID_OPTION);
+        $seller = $this->sellerRepository->getById($sellerId);
+        $output->writeln(
+            'Seller name : '.$seller->getName()
+        );
+    }
+}
