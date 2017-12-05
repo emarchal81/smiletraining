937fa634eb1a9413cf6cd430339ba90a6f6e9455
Eric Marchal <e.marchal@truffaut.com>
2017-12-05 10:21:25 -0500

    permissions sans smile

--- architecture/script/permissions.sh
+++ architecture/script/permissions.sh
@@ -4,23 +4,23 @@ echo ""
 echo "Update Permissions"
 echo ""
 
-chown -R smile.www-data /var/www/magento2/app/etc
+chown -R www-data /var/www/magento2/app/etc
 chmod -R 664 /var/www/magento2/app/etc
 chmod -R +X /var/www/magento2/app/etc
 
-chown -R smile.www-data /var/www/magento2/pub/static
+chown -R www-data /var/www/magento2/pub/static
 chmod -R 664 /var/www/magento2/pub/static
 chmod -R +X /var/www/magento2/pub/static
 
-chown -R smile.www-data /var/www/magento2/pub/media
+chown -R www-data /var/www/magento2/pub/media
 chmod -R 664 /var/www/magento2/pub/media
 chmod -R +X /var/www/magento2/pub/media
 
-chown -R smile.www-data /var/www/magento2/var
+chown -R www-data /var/www/magento2/var
 chmod -R 664 /var/www/magento2/var
 chmod -R +X /var/www/magento2/var
 
-chown -R smile.www-data /var/www/magento2/generated
+chown -R www-data /var/www/magento2/generated
 chmod -R 664 /var/www/magento2/generated
 chmod -R +X /var/www/magento2/generated
 
